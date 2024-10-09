052f0f178ceee80535554f676f63bb3757093488
= <=>
2024-10-09 15:07:05 +0700

    JS07_Praktikum5-2

--- JS07_PHP-Form/regex.php
+++ JS07_PHP-Form/regex.php
@@ -1,10 +1,10 @@
 <?php
-$pattern = '/[a-z]/'; // Cocokkan huruf kecil.
-$text = 'This is a Sample Text.';
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit
+$text = 'There are 123 apples.';
 
-if (preg_match($pattern, $text)) {
-    echo 'Huruf kecil ditemukan!';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
 } else {
-    echo 'Tidak ada huruf kecil!';
+    echo 'Tidak ada cocok!';
 }
 ?>
