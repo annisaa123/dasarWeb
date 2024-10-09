085d22f4435013f6ef00914dd825a56f94eb6d05
= <=>
2024-10-09 12:44:51 +0700

    JS07_Praktikum1-2

--- JS07_PHP-Form/isset.php
+++ JS07_PHP-Form/isset.php
@@ -2,9 +2,16 @@
 $umur;
 
 if (isset($umur) && $umur >= 18) {
-    echo "Anda sudah dewasa.";
+    echo "Anda sudah dewasa.<br>";
 } else {
-    echo "Anda belum dewasa.";
+    echo "Anda belum dewasa.<br>";
 }
 
+$data = array("nama" => "Jane", "usia" => 25);
+
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"] . "<br>";
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.<br>";
+}
 ?>
