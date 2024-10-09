31b1b7039564b5ebecb3e822429e54fd81531c6f
= <=>
2024-10-09 15:16:07 +0700

    JS07_Praktikum5-3

--- JS07_PHP-Form/regex.php
+++ JS07_PHP-Form/regex.php
@@ -3,8 +3,15 @@ $pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit
 $text = 'There are 123 apples.';
 
 if (preg_match($pattern, $text, $matches)) {
-    echo "Cocokkan: " . $matches[0];
+    echo "Cocokkan: " . $matches[0] . "<br>";
 } else {
-    echo 'Tidak ada cocok!';
+    echo 'Tidak ada cocok!' . "<br>";
 }
+
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I like apple pie.';
+$new_text = preg_replace($pattern, $replacement, $text);
+
+echo $new_text;
 ?>
