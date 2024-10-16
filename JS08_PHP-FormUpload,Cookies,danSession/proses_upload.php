5b22041a1430639b40cb6d26d37f8098cb5df633
= <=>
2024-10-16 14:07:23 +0700

    JS08_Praktikum 2-1

--- JS08_PHP-FormUpload,Cookies,danSession/proses_upload.php
+++ JS08_PHP-FormUpload,Cookies,danSession/proses_upload.php
@@ -4,25 +4,25 @@ $targetDirectory = "documents/";
 
 // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
-mkdir($targetDirectory, 0777, true);
+    mkdir($targetDirectory, 0777, true);
 }
 
-if ($_FILES['files']['name'][0]) {
+if (isset($_FILES['files']) && $_FILES['files']['name'][0]) {
     $totalFiles = count($_FILES['files']['name']);
 
-// Loop melalui semua file yang digunggah
-for ($i = 0; $i < $totalFiles; $i++) {
-$fileName = $_FILES['files']['name'][$i];
-$targetFile = $targetDirectory . $fileName;
+    // Loop melalui semua file yang digunggah
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
 
-// Pindahkan file yang digunggah ke direktori penyimpanan
-if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-    echo "File $fileName berhasil diunggah.";
+        // Pindahkan file yang digunggah ke direktori penyimpanan
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+            echo "File $fileName berhasil diunggah.<br>";
+        } else {
+            echo "Gagal mengunggah file $fileName.<br>";
+        }
+    }
 } else {
-    echo "Gagal mengunggah file $fileName.<br>";
-}
-}
-} else {
-echo "Tidak ada file yang diunggah.";
+    echo "Tidak ada file yang diunggah.";
 }
 ?>
