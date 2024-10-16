1c5caca8a8aa2b4220ff81b8df0efef3c45e83c3
= <=>
2024-10-16 13:31:23 +0700

    JS08_Praktikum 1-2

---
+++ JS08_PHP-FormUpload,Cookies,danSession/proses_upload.php
@@ -0,0 +1,28 @@
+<?php
+// Lokasi penyimpanan file yang digunggah
+$targetDirectory = "documents/";
+
+// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+if (!file_exists($targetDirectory)) {
+mkdir($targetDirectory, 0777, true);
+}
+
+if ($_FILES['files']['name'][0]) {
+    $totalFiles = count($_FILES['files']['name']);
+
+// Loop melalui semua file yang digunggah
+for ($i = 0; $i < $totalFiles; $i++) {
+$fileName = $_FILES['files']['name'][$i];
+$targetFile = $targetDirectory . $fileName;
+
+// Pindahkan file yang digunggah ke direktori penyimpanan
+if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+    echo "File $fileName berhasil diunggah.";
+} else {
+    echo "Gagal mengunggah file $fileName.<br>";
+}
+}
+} else {
+echo "Tidak ada file yang diunggah.";
+}
+?>
