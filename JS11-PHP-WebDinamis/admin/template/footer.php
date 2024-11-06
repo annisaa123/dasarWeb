49182b442db4834748a5149728ade666637c7de3
= <=>
2024-11-06 15:59:31 +0700

    JS11-Praktikum3

---
+++ JS11-PHP-WebDinamis/admin/template/footer.php
@@ -0,0 +1,7 @@
+<script src ="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src ="https://cdn.jsdelivr.net/npm/chart.js@4.3.3/dist/chart.umd.min.js" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous"></script>
+<script src ="assets/custom/dashboard.js"></script>
+</body>
+
+</html>
