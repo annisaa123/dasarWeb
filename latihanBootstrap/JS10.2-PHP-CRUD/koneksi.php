b5eefb25024344e74ed3e29474fac1a4ef01e340
= <=>
2024-11-21 23:59:00 +0700

    JS13-AdminLTE

--- latihanBootstrap/JS10.2-PHP-CRUD/koneksi.php
+++ latihanBootstrap/JS10.2-PHP-CRUD/koneksi.php
@@ -1,9 +1,15 @@
-<?php 
-// Koneksi ke database
-$koneksi = mysqli_connect("localhost", "root", "", "prakwebdb");
+<?php
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB1', 'prakwebdb');
 
-// Periksa koneksi
-if (mysqli_connect_errno()) {
-    die("Koneksi database gagal: ". mysqli_connect_error());
-}
-?>
+$db1 = new mysqli(HOST, USER, PASS, DB1);
+
+//p1-5
+// $koneksi = mysqli_connect("localhost", "root", "", "prakwebdb");
+
+// //periksa koneksi
+// if (mysqli_connect_errno()) {
+//     die("Koneksi database gagal: " . mysqli_connect_error());
+// }
